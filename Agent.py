f06269fadadc73f754e08ee8bcb54158d5dfb91f
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-10-24 06:26:10 +0200

    feat: solution for tennis

--- Agent.py
+++ Agent.py
@@ -16,11 +16,11 @@ GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor
 LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0.0001   # L2 weight decay
+WEIGHT_DECAY = 0   # L2 weight decay
 EPSILON_MAX = 1.0
 EPSILON_MIN = 0.1
-EPSILON_DECAY = 1e-6
-LEARN_START = 20000
+EPSILON_DECAY = 0
+LEARN_START = 0
 UPDATE_EVERY = 20
 UPDATES_PER_STEP = 10
 
